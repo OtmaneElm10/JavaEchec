f8007a7f93a62197508724bb61670cd9007502ba
Xandeers <nedim.tr.pro@gmail.com>
2025-04-03 20:52:22 +0000

    ajout cavalier

--- code/src/modele/jeu/Piece.java
+++ code/src/modele/jeu/Piece.java
@@ -13,19 +13,30 @@ public abstract class Piece {
     protected Case c;
     protected Plateau plateau;
     protected DecorateurCasesAccessibles casesAccessibles;
+    protected boolean blanc ;
 
-    public Piece(Plateau _plateau) {
+    public Piece(Plateau _plateau , boolean _blanc) {
         plateau = _plateau;
+        blanc = _blanc; 
 
     }
 
     public void quitterCase() {
+
         c.quitterLaCase();
     }
+
+    public boolean estBlanc() {
+
+        return blanc;
+    }
+
     public void allerSurCase(Case _c) {
         if (c != null) {
+
             quitterCase();
         }
+        
         c = _c;
         plateau.arriverCase(c, this);
     }
---
+++ code/src/modele/jeu/cavalier.java
@@ -0,0 +1,13 @@
+package modele.jeu;
+
+import modele.plateau.*;
+
+public class Cavalier extends Piece {
+
+    public Cavalier(Plateau plateau ,boolean est_blanc) {
+        super(plateau,est_blanc);
+
+        
+        casesAccessibles = new DecorateurCasesCavalier(null, plateau, this, 1); // avec le limiteur a 1 meme si il est pas utilisé 
+    }
+}

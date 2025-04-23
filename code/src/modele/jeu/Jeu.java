b088f2e2914df7323fd5a434b3c6d1742894487d
xanders <nedim.trad3@gmail.com>
2025-04-23 10:59:07 +0000

    ajout en echec

--- code/src/modele/jeu/Jeu.java
+++ code/src/modele/jeu/Jeu.java
@@ -49,15 +49,26 @@ public class Jeu extends Thread{
 
 
     public void appliquerCoup(Coup coup) {
+
+        Piece pieceDeplacee = coup.dep.getPiece();
+
     if (coup.dep.getPiece() != null) {
         List<Case> casesPossibles = coup.dep.getPiece().casesAccessibles.getCasesAccessibles();
         if (casesPossibles.contains(coup.arr)) {
             // Si la destination est accessible, on applique le coup
             plateau.deplacerPiece(coup.dep, coup.arr);
+
+            boolean estEchec = estEnEchec(pieceDeplacee.estBlanc());
+            if (estEchec) {
+                System.out.println("Attention : votre roi est en échec !");
+            }
         } else {
             System.out.println("Coup invalide !");
         }
     }
+  
+}
+
 }
 
     public void run() {
@@ -73,5 +84,52 @@ public class Jeu extends Thread{
 
     }
 
+    private boolean caseEstAttaquee(Point position, boolean verif) {
 
+        for (int x = 0; x < Plateau.SIZE_X; x++) {
+            for (int y = 0; y < Plateau.SIZE_Y; y++) {
+
+                Piece p = plateau.getCases()[x][y].getPiece();
+                if (p != null && p.estBlanc() == verif) {
+
+                    if (p.casesAccessibles.getCasesAccessibles().contains(plateau.getCases()[position.x][position.y])) 
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+
+    public boolean estEnEchec(boolean est_roi) {
+
+        Roi roi = trouverRoi(est_roi);
+        if (roi == null) {
+
+            return false; // probleme 
+        }
+        
+        Point positionRoi = plateau.getPositionCase(roi.getCase());
+        return caseEstAttaquee(positionRoi, !est_roi); 
+    }
+    
+    private Roi trouverRoi(boolean estBlanc) {
+
+        for (int x = 0; x < Plateau.SIZE_X; x++) {
+
+            for (int y = 0; y < Plateau.SIZE_Y; y++) {
+
+                Piece p = plateau.getCases()[x][y].getPiece();
+
+                if (p instanceof Roi && p.estBlanc() == estBlanc) {
+
+                    return (Roi) p;
+                }
+            }
+        }
+        return null;
+    }
+    
 }
